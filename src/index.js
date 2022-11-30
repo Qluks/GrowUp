61ad4f4fb0725bb904fb74b78a03f76bc103e4e6
Thaís Tawany <99848191+thais-rds18@users.noreply.github.com>
2022-11-30 19:08:16 -0300

    ULTIMO TESTE

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import BlipChat from 'https://unpkg.com/blip-chat-widget';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
